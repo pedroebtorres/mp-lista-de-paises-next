35ac5d023711162da9ea75a4278fa90e57d6132f
pedroebtorres <pebtorres@hotmail.com>
2023-10-11 18:13:40 -0400

    Countries in Which Speak the Same Language

--- spiskom-stran/src/app/countries/CountryDetails.tsx
+++ spiskom-stran/src/app/countries/CountryDetails.tsx
@@ -3,6 +3,7 @@ import { CountryData } from "@/interfaces/CountryData"
 import CountryLanguages from "../../components/CountryLanguages";
 import Image from "next/image";
 import NeighborsCountries from "./NeighborsCountries";
+import SameLanguageCountries from "./SameLanguageCountries";
 import Link from 'next/link';
 import arrow from "../../images/arrow.svg";
 
@@ -16,7 +17,9 @@ const imageStyle = {
     height: "275px",
 }
 const CountryDetails = (props: Props) => {
+    const languages = Object.keys(props.country.languages);
     console.log(props.country);
+    console.log(languages);
     return (
         <div className="flex-1 text-center mt-3">
             <h1 className=" text-5xl text-gray-800 font-bold">{props.country.name.common}</h1>
@@ -29,7 +32,7 @@ const CountryDetails = (props: Props) => {
             <div className="flex justify-center mt-7 bg-white ml-72 mr-64 p-10 rounded-xl">
 
                 <div className="flex-1 flex-col text-start">
-                    <p className='my-4'><strong>🏙️ Capital:</strong> {props.country.capital[0]}</p>
+                    {props.country.capital && <p className='my-4'><strong>🏙️ Capital:</strong> {props.country.capital[0]}</p>}
                     <p className='my-4'><strong>🗺️ Continente: </strong>{props.country.region}</p>
                     <p className='my-4'><strong>👨‍👩‍👧‍👦 População: </strong>{(props.country.population / 1000000).toFixed(1)}M</p>
                     <CountryLanguages languages={props.country.languages}/>
@@ -51,6 +54,10 @@ const CountryDetails = (props: Props) => {
             <div>
             <h1 className="text-3xl text-gray-800 font-bold text-start ml-72 my-24 ">Países que fazem fronteira</h1>
                     <NeighborsCountries borders={props.country.borders}/>
+            </div>
+            <div>
+            <h1 className="text-3xl text-gray-800 font-bold text-start ml-72 my-24 ">Países que falam o mesmo idioma</h1>
+                    <SameLanguageCountries languages={Object.keys(props.country.languages)}/>
             </div>
         </div>
     )
